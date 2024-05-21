75813f81c684b72880ff22b161585abb9dac688f
theEdgeBreaker <thesanjana2002@gmail.com>
2024-05-21 23:05:31 +0530

    change sidebar code and layout

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -2,107 +2,52 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-import { AiOutlineHome } from "react-icons/ai";
-import { MdOutlineLocalMovies } from "react-icons/md";
-import { PiTelevisionSimpleBold } from "react-icons/pi";
-import { PiFireBold } from "react-icons/pi";
-import { FiHeart } from "react-icons/fi";
-import { MdCalendarMonth } from "react-icons/md";
-import { FaCircleUser } from "react-icons/fa6";
+import { SIDEBAR_LINKS } from "@/config/sidebar";
 
-const Sidebar = () => {
+const Sidebar = ({ showSidebar }) => {
   return (
-    <div className=" bg-secondary min-h-screen md:px-5 lg:px-7 py-12 border-r-[1px] border-[#1e1e1f] pb-6 hidden md:block ">
+    <div
+      className={`bg-secondary min-h-screen md:px-1 lg:px-2 py-12 border-r-[1px] ${
+        showSidebar ? "w-[14rem]" : "w-[5rem]"
+      } border-[#1e1e1f] pb-6 hidden md:block transition-all duration-300`}
+    >
       <div className="flex flex-col gap-3">
-        <div className="flex flex-col gap-10">
-          <div className="flex items-center gap-7">
+        <div className={`flex flex-col gap-10 w-max`}>
+          <div className="flex items-center  mb-6  md:px-4 lg:px-3">
             {/* User */}
-            <FaCircleUser className="text-white text-4xl" />
             {/* Logo */}
             <Image
               src="/logo.png"
               alt="StreamIT Logo"
-              width={72}
-              height={16}
-              className=" 
-              sm:h-[1.5rem] sm:w-[5.5rem]
-              lg:h-[2rem] lg:w-[7rem] cursor-pointer
-              "
+              width={46.5}
+              height={69.5}
+              className="cursor-pointer object-fit-cover"
             />
+            {showSidebar && (
+              <span className="text-maroonVelvet font-bold text-2xl">
+                STREAMIT
+              </span>
+            )}
           </div>
-
-          <h4 className=" text-gray-500 font-semibold md:text-xs lg:text-sm">
-            News Feed
-          </h4>
         </div>
 
-        <div className=" text-white flex flex-col gap-5 md:text-sm lg:text-lg border-b-[1px] border-[#1e1e1f] pb-6 font-semibold">
-          <Link href="/">
-            <div className="flex flex-row md:gap-4 lg:gap-6 items-center hover:bg-maroonVelvet ">
-              <AiOutlineHome className=" md:text-xl lg:text-2xl" />
-              <p>Home</p>
-            </div>
-          </Link>
-
-          <Link href="/movies">
-            <div className="flex flex-row md:gap-4 lg:gap-6 items-center hover:bg-maroonVelvet ">
-              <MdOutlineLocalMovies className=" md:text-xl lg:text-2xl" />
-              <p>Movies</p>
-            </div>
-          </Link>
-
-          <Link href="/tv-shows ">
-            <div className="flex flex-row md:gap-4 lg:gap-6 items-center hover:bg-maroonVelvet ">
-              <PiTelevisionSimpleBold className=" md:text-xl lg:text-2xl" />
-              <p>TV Shows</p>
-            </div>
-          </Link>
-
-          <Link href="/trending">
-            <div className="flex flex-row md:gap-4 lg:gap-6 items-center hover:bg-maroonVelvet ">
-              <PiFireBold className=" md:text-xl lg:text-2xl" />
-              <p>Trending</p>
-            </div>
-          </Link>
-
-          <Link href="/watchlist">
-            <div className="flex flex-row md:gap-4 lg:gap-6 items-center hover:bg-maroonVelvet ">
-              <FiHeart className=" md:text-xl lg:text-2xl" />
-              <p>Watchlist</p>
-            </div>
-          </Link>
-
-          <Link href="/coming-soon">
-            <div className="flex flex-row md:gap-4 lg:gap-6 items-center hover:bg-maroonVelvet ">
-              <MdCalendarMonth className=" md:text-xl lg:text-2xl" />
-              <p>Coming Soon</p>
-            </div>
-          </Link>
-
-          {/* <div className="flex flex-row gap md:gap-4 lg:gap-6 items-center">
-            <MdOutlineLocalMovies className=" md:text-xl lg:text-2xl" />
-            <Link href="/movies">Movies</Link>
+        <div
+          className={` text-white flex flex-col gap-3  border-b-[1px] border-[#1e1e1f] pb-6 
+          `}
+        >
+          <div className={`flex flex-col gap-3`}>
+            {SIDEBAR_LINKS.map((link) => (
+              <Link key={link.name} href={link.url}>
+                <div
+                  className={`flex items-center rounded-2xl p-2 md:p-4 lg:px-3 lg:py-2.5 
+                  hover:bg-maroonVelvet transition-all duration-300`}
+                >
+                  <p className="text-3xl">{link.icon}</p>
+                  {showSidebar && <p className="ml-2 text-lg">{link.name}</p>}
+                </div>
+              </Link>
+            ))}
           </div>
-
-          <div className="flex flex-row md:gap-4 lg:gap-6 items-center">
-            <PiTelevisionSimpleBold className=" md:text-xl lg:text-2xl" />
-            <Link href="/tv-shows ">TV Shows</Link>
-          </div>
-
-          <div className="flex flex-row md:gap-4 lg:gap-6 items-center">
-            <PiFireBold className=" md:text-xl lg:text-2xl" />
-            <Link href="/trending">Trending</Link>
-          </div>
-
-          <div className="flex flex-row md:gap-4 lg:gap-6 items-center">
-            <FiHeart className=" md:text-xl lg:text-2xl" />
-            <Link href="/watchlist">Watchlist</Link>
-          </div>
-
-          <div className="flex flex-row md:gap-4 lg:gap-6 items-center">
-            <MdCalendarMonth className=" md:text-xl lg:text-2xl" />
-            <Link href="/coming-soon">Coming Soon</Link>
-          </div> */}
         </div>
       </div>
     </div>
