f30a65056bdc1b7460defd72e7794d4bb0fbb709
theEdgeBreaker <thesanjana2002@gmail.com>
2024-05-21 23:03:10 +0530

    make a constant file and have all the constants in it

---
+++ src/config/constants.js
@@ -0,0 +1,7 @@
+// Sidebar
+export const HOME = "Home";
+export const MOVIES = "Movies";
+export const TV_SHOWS = "TV Shows";
+export const TRENDING = "Trending";
+export const WATCHLIST = "Watchlist";
+export const COMING_SOON = "Coming Soon";
